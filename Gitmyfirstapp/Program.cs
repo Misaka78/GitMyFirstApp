0340b281eba5770a46f9256ec9e2df4f70b17ab9
Maria <Maria@DESKTOP-2OU4NN7>
2022-01-18 12:59:56 +0200

    Add Minus func

--- Gitmyfirstapp/Program.cs
+++ Gitmyfirstapp/Program.cs
@@ -13,12 +13,18 @@ namespace Gitmyfirstapp
             Console.WriteLine("Hello World");
            // int c = Add(2,8);
             Console.WriteLine(Add(2,8));
+            Console.WriteLine(Minus(6, 4));
             Console.ReadKey();
         }
 
         static int Add(int a,int b)
         {
            int c= a + b;
+            return c;
+        }
+        static int Minus(int a, int b)
+        {
+            int c = a - b;
             return c;
         }
     }
